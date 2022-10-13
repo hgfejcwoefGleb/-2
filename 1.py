843bf5b8a2de984a3995007174e4bcfadf76a5a3
Глеб Пилипенко <gipilipenko@edu.hse.ru>
2022-10-14 00:50:38 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+print("Добро пожаловать в первое домашнее задание")
+name = str(input("Введите имя: "))
+surname = str(input("Введите фамилию: "))
+age = float(input("Введите возраст: "))
+print(name,"_",surname,"_",age)
+name2= surname
+surname2 = name
+age = age+60
+print(name2,"_",surname2,"_",age)
